7d518b9fb303bb500afe3437652b9ce49111283a
Alejandro <eidalcas16@gmail.com>
2023-09-22 18:56:54 -0500

    se completo la clase del primer proyecto de react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,187 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { v4 as uuid } from "uuid"; //crea identificadores diferentes
+import "./App.css";
+import Header from "./componentes/Header/Header";
+import Formulario from "./componentes/Formulario/Formulario";
+import MiOrg from "./componentes/MiOrg";
+import Equipo from "./componentes/Equipo";
+// import Colaborador from "./componentes/Colaborador";
+import Footer from "./componentes/Footer";
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar] = useState(false);
+  // ternario o ternary operator --> condicion ? seMuestra : noSeMuestra
+  const [colaboradores, actualizarColaboradores] = useState([
+    {
+      id: uuid(),
+      equipo: "Front End",
+      foto: "https://github.com/harlandlohora.png",
+      nombre: "Harland Lohora",
+      puesto: "Instructor",
+      fav: true,
+    },
+    {
+      id: uuid(),
+      equipo: "Programación",
+      foto: "https://github.com/genesysR-dev.png",
+      nombre: "Genesys Rondón",
+      puesto: "Desarrolladora de software e instructora",
+      fav: false,
+    },
+
+    {
+      id: uuid(),
+      equipo: "UX y Diseño",
+      foto: "https://github.com/JeanmarieAluraLatam.png",
+      nombre: "Jeanmarie Quijada",
+      puesto: "Instructora en Alura Latam",
+      fav: false,
+    },
+    {
+      id: uuid(),
+      equipo: "Programación",
+      foto: "https://github.com/christianpva.png",
+      nombre: "Christian Velasco",
+      puesto: "Head de Alura e instructor",
+      fav: false,
+    },
+    {
+      id: uuid(),
+      equipo: "Innovación y Gestión",
+      foto: "https://github.com/JoseDarioGonzalezCha.png",
+      nombre: "Jose Gonzalez",
+      puesto: "Dev Fullstack",
+      fav: false,
+    },
+  ]);
+  const [equipos, actualizarEquipos] = useState([
+    // Lista de Equipos
+    {
+      id: uuid(),
+      titulo: "Programación",
+      colorPrimario: "#57c278",
+      colorSecundario: "#d9f7e9",
+    },
+    {
+      id: uuid(),
+      titulo: "Front End",
+      colorPrimario: "#82cffa",
+      colorSecundario: "#e8f8ff",
+    },
+    {
+      id: uuid(),
+      titulo: "Data science",
+      colorPrimario: "#a6d157",
+      colorSecundario: "#f0f8e2",
+    },
+    {
+      id: uuid(),
+      titulo: "Devops",
+      colorPrimario: "#e06b69",
+      colorSecundario: "#fde7e8",
+    },
+    {
+      id: uuid(),
+      titulo: "UX y Diseño",
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5",
+    },
+    {
+      id: uuid(),
+      titulo: "Móvil",
+      colorPrimario: "#ffba05",
+      colorSecundario: "#fff5d9",
+    },
+    {
+      id: uuid(),
+      titulo: "Innovación y Gestión",
+      colorPrimario: "#ff8a29",
+      colorSecundario: "#ffeedf",
+    },
+  ]);
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario);
+  };
+
+  // Registrar colaborador
+  const registrarColaborador = (colaborador) => {
+    console.log("nuevo colaborador", colaborador);
+    // spread operator
+    actualizarColaboradores([...colaboradores, colaborador]);
+  };
+
+  // Eliminar colaborador
+  const eliminarColaborador = (id) => {
+    console.log("Eliminar colaborador", id);
+    const nuevosColaboradores = colaboradores.filter(
+      (colaborador) => colaborador.id !== id
+      //esta linea podria ser de la siguiente manera
+      //(colaborador) => { return colaborador.id !== id}
+    );
+    actualizarColaboradores(nuevosColaboradores);
+  };
+
+  // actualizar color de equipo
+  const actualizarColor = (color, id) => {
+    console.log("actualizr: ", color, id);
+    const equiposActualizados = equipos.map((equipo) => {
+      if (equipo.id === id) {
+        equipo.colorPrimario = color;
+      }
+      return equipo;
+    });
+
+    actualizarEquipos(equiposActualizados);
+  };
+
+  // crear equipo
+  const crearEquipo = (nuevoEquipo) => {
+    console.log("Nuevo equipo");
+    actualizarEquipos([...equipos, { ...nuevoEquipo, id: uuid() }]);
+  };
+
+  const like = (id) => {
+    console.log("like");
+    const colaboradoresActualizados = colaboradores.map((colaborador) => {
+      if (colaborador.id === id) {
+        colaborador.fav = !colaborador.fav;
+      }
+      return colaborador;
+    });
+
+    actualizarColaboradores(colaboradoresActualizados);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      {/* {mostrarFormulario === true ? <Formulario /> : <div />}   esto es lo mismo que la siguiente linea*/}
+      {mostrarFormulario && (
+        <Formulario
+          equipos={equipos.map((equipo) => equipo.titulo)}
+          registrarColaborador={registrarColaborador}
+          crearEquipo={crearEquipo}
+        />
+      )}
+
+      <MiOrg cambiarMostrar={cambiarMostrar} />
+
+      {equipos.map((equipo) => {
+        return (
+          <Equipo
+            datos={equipo}
+            key={equipo.titulo}
+            colaboradores={colaboradores.filter(
+              (colaborador) => colaborador.equipo === equipo.titulo
+            )}
+            eliminarColaborador={eliminarColaborador}
+            actualizarColor={actualizarColor}
+            like={like}
+          />
+        );
+      })}
+      <Footer />
     </div>
   );
 }
